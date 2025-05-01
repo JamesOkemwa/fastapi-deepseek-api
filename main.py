a82b4753d6d24fcc342997f83f458e4cdaf8711b
JamesOkemwa <okemwasjames@gmail.com>
2025-05-01 12:47:42 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from openai import OpenAI
+
+client = OpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key=""
+)
+
+def translate_text(text, target_language):
+    prompt = f"Translate the following text to {target_language}: {text}"
+    print(prompt)
+    completion = client.chat.completions.create(
+        model='deepseek/deepseek-r1:free',
+        messages = [
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ]
+    )
+
+    return completion.choices[0].message.content
