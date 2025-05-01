ccda6123c9b3ea5c2a5bdf4a91bb8e256c11b992
JamesOkemwa <okemwasjames@gmail.com>
2025-05-01 14:32:51 +0200

    Load environment variables

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+# load environment variables
+load_dotenv()
 
 client = OpenAI(
     base_url="https://openrouter.ai/api/v1",
-    api_key=""
+    api_key=os.getenv("OPENROUTER_API_KEY")
 )
 
 def translate_text(text, target_language):
     prompt = f"Translate the following text to {target_language}: {text}"
-    print(prompt)
     completion = client.chat.completions.create(
         model='deepseek/deepseek-r1:free',
         messages = [
